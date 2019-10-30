750163ad8845b026d1f8fd966e01ee345ac9af1c
Amazon Unicorn(aka Kayla) <strait750@gmail.com>
2019-10-30 22:25:27 +0000

    debug

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-$("button").click(function(){
-    $(".input").val("")
-}
+$( "document" ).ready(function() {
+    $("button").click(function(){
+        $(".output").text("");
+        var sentence =$(".input").val();
+        var words = sentence.split(' ');
+        var vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+        words.forEach(function(word){
+            var firstLetter=word[0].toLowerCase();
+            if (vowels.includes(firstLetter)) {
+                console.log(true);
+                var vowelWord= word + "ay ";
+                $('.output').append(vowelWord);
+            }else{
+                  console.log(false);
+                var conWord= word.slice(1) + firstLetter + "ay ";
+                $('.output').append(conWord);
+            }
+
+            });
+
+        });
 
 
-
-
-
-
-});
+    });
